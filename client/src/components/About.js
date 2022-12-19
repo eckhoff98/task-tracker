88a05b39bb40a89f011c21d6218fea832d9130b5
eckhoff98 <eckhoff98@outlook.com>
2022-12-18 20:31:41 -0700

    about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -4,7 +4,9 @@ const About = () => {
     return (
         <>
             <h1>About</h1>
-            <p>Task Tracker is a simple app for keeping track of tasks, with a time and reminder option.</p>
+            <p>Task Tracker is my first app made with <a href="https://reactjs.org/">React</a> to learn the ins and outs of the Javascript library.</p>
+            <p>The backend and authentication of the Task Tracker is handled with <a href="https://pocketbase.io/">Pocketbase</a>.</p>
+            <p>Here is a link to the project on <a href="https://github.com/eckhoff98/TaskTracker">Github</a>.</p>
         </>
     )
 }
