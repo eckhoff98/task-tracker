4a004f08bc75eee70195e93296973c756eee16b6
eckhoff98 <eckhoff98@outlook.com>
2023-01-03 04:55:03 -0700

    read me and about

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Link to test out the app: https://fireshiptutorial-71e6c.web.app
 The frontend is a simple React project created in Node with create-react-app.
 ## Backend
 The backend is handled with Firebase. Its used to authnticate and store users' info and tasks
+## Styling
+The styling is mainly handled with Bootstrap and Bootswatch with a little bit of custom CSS here and there.
+Bootstrap: https://getbootstrap.com/
+Bootswatch: https://bootswatch.com/
+
--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,10 @@ const About = () => {
             <p>Task Tracker is my first app made with <a href="https://reactjs.org/">React</a> to learn the ins and outs of the Javascript library.</p>
             <p>The backend and authentication of the Task Tracker is handled with <a href="https://firebase.google.com/">Firebase</a>.</p>
             <p>Here is a link to the project on <a href="https://github.com/eckhoff98/TaskTracker">Github</a>.</p>
+            <p>
+                The styling is mainly handled with <a href="https://getbootstrap.com/">Bootstrap</a> and <a href="https://bootswatch.com/">Bootswatch</a>
+                with a little bit of custom CSS here and there.
+            </p>
         </>
     )
 }
